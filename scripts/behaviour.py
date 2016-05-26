4a1594076d43165e652951fec90ee81bec101936
Sam Pfeiffer <sam.pfeiffer@pal-robotics.com>
2016-05-26 08:06:25 +0000

    Added pointhead function

--- scripts/behaviour.py
+++ scripts/behaviour.py
@@ -3,6 +3,8 @@
 import rospy
 from pal_interaction_msgs.msg import TtsAction, TtsGoal, TtsResult, TtsFeedback
 from pal_detection_msgs.msg import FaceDetections, FaceDetection
+from control_msgs.msg import PointHeadAction, PointHeadGoal, PointHeadResult, PointHeadFeedback
+
 from actionlib import SimpleActionClient
 
 
@@ -10,8 +12,12 @@ class ChipBehaviour(object):
     def __init__(self):
         rospy.loginfo("Initializing ChipBehaviour")
 
-        self.ac = SimpleActionClient('/tts', TtsAction)
-        self.ac.wait_for_server()
+        self.tts_ac = SimpleActionClient('/tts', TtsAction)
+        self.tts_ac.wait_for_server()
+
+        self.point_head_ac = SimpleActionClient('/head_controller/point_head_action',
+                                                PointHeadAction)
+        self.point_head_ac.wait_for_server(rospy.Duration(10.0))
 
         self.faces_sub = rospy.Subscriber('/pal_face/faces',
                                           FaceDetections,
@@ -24,6 +30,20 @@ class ChipBehaviour(object):
             rospy.loginfo("Face msg: " + str(msg.faces))
             self.say("Oh, face")
 
+    def look_at(self, x, y, z):
+        phg = PointHeadGoal()
+        phg.target.header.frame_id = '/stereo_optical_frame'
+        phg.target.point.x = x
+        phg.target.point.y = y
+        phg.target.point.z = z
+
+        phg.pointing_axis.z = 1.0
+        phg.pointing_frame = phg.target.header.frame_id
+        phg.min_duration = rospy.Duration(2.0)
+        phg.max_velocity = 1.0
+
+        self.point_head_ac.send_goal_and_wait(phg)
+
     def say(self, text):
         rospy.loginfo("Creating goal with text: " + text)
         goal = TtsGoal()
@@ -31,7 +51,7 @@ class ChipBehaviour(object):
         goal.rawtext.lang_id = 'en_GB'
 
         rospy.loginfo("Sending goal!")
-        self.ac.send_goal_and_wait(goal)
+        self.tts_ac.send_goal_and_wait(goal)
         rospy.loginfo("We are done!")
 
 
@@ -39,4 +59,5 @@ if __name__ == '__main__':
     rospy.init_node('robot_talk', anonymous=True)
     cb = ChipBehaviour()
     #cb.say("Sam is an awesome teacher")
+    cb.look_at(-2.0, 0.0, 2.0)
     rospy.spin()
